90dbcf9d8f2dce562a3c40ff8749e741d3e07e30
Domenic Helfenstein <d.helfenstein@calitime.ch>
2020-07-29 08:38:03 +0200

    session class code refactoring

--- lib/session.ts
+++ lib/session.ts
@@ -12,25 +12,32 @@ export class Session<T> {
         private repo: ISessionRepository<T> = new InMemorySessionRepository<T>()) { }
 
     public sessionHandling = async (context: Context, next: () => Promise<void>) => {
-        let sessionId: string;
-        const cookieSessionId = context.cookies.get(SESSION_ID);
-        if (cookieSessionId == undefined) {
-            const initialState = this.initState();
-            sessionId = v4.generate();
-            await this.repo.set(sessionId, initialState);
-            context.cookies.set(SESSION_ID, sessionId, this.cookieOptions);
-            context.state.session = initialState;
-        } else {
-            sessionId = cookieSessionId;
-            context.state.session = await this.repo.get(sessionId)
-                .catch(async _ => {
-                    const initialState = this.initState();
-                    await this.repo.set(sessionId, initialState);
-                    return initialState;
-                });
-        }
+        const sessionIdFromCookie = context.cookies.get(SESSION_ID);
+        const sessionId = sessionIdFromCookie == undefined
+            ? await this.noSessionCookieAvailable(context)
+            : await this.sessionCookieAvailable(context, sessionIdFromCookie);
 
         await next();
+
         await this.repo.set(sessionId, context.state.session);
     };
+
+    private noSessionCookieAvailable = async (context: Context) => {
+        const initialState = this.initState();
+        const sessionId = v4.generate();
+        await this.repo.set(sessionId, initialState);
+        context.cookies.set(SESSION_ID, sessionId, this.cookieOptions);
+        context.state.session = initialState;
+        return sessionId;
+    }
+
+    private sessionCookieAvailable = async (context: Context, sessionId: string) => {
+        context.state.session = await this.repo.get(sessionId)
+            .catch(async _ => {
+                const initialState = this.initState();
+                await this.repo.set(sessionId, initialState);
+                return initialState;
+            });
+        return sessionId;
+    }
 }
